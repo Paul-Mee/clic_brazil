a4e4a0c6120d043834fd33c354955512ccd4f515
nealalexander <neal.alexander@lshtm.ac.uk>
2021-05-25 10:39:44 -0500

    updating folders for use by Neal

--- CLIC_Brazil_peak_pred_implementation.R
+++ CLIC_Brazil_peak_pred_implementation.R
@@ -9,7 +9,11 @@
 ##############
 dir_scripts <- "C:/github/clic_brazil/"
 
-source (paste0(dir_scripts,"CLIC_Brazil_Script_directories.R"))
+if(Sys.info()[['user']]=="eidenale"){
+   dir_scripts <- "C:/Users/eidenale/Documents/clic_brazil/"
+}
+
+source(paste0(dir_scripts,"CLIC_Brazil_Script_directories.R"))
 
 # loads function to be used for peak prediction
 source(paste0(dir_scripts,"CLIC_Brazil_peak_pred_function.R"))
