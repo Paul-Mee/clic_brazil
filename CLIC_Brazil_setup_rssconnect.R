bc717cbc6768b01761778f40f93e710e37b4ae79
Paul  Mee <phpupmee@lshtm.ac.uk>
2021-06-08 12:00:10 +0100

    New files for deploy app

---
+++ CLIC_Brazil_setup_rssconnect.R
@@ -0,0 +1,3 @@
+### This is run once to connect the local machine to the CMMID server 
+library(rsconnect)
+rsconnect::setAccountInfo(name='cmmid-lshtm', token='52AC30B67FF51C55692130E56A5F183A', secret='FaRyuEVMUDN5MzEFCr3DOl5M9+8qrFOUuuAf9ofc')
