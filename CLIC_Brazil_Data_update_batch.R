b4be1f0e7dcc96d1b08742ea9b7167670d6fb881
Paul  Mee <phpupmee@lshtm.ac.uk>
2021-12-13 16:04:41 +0000

    code update for testing

--- CLIC_Brazil_Data_update_batch.R
+++ CLIC_Brazil_Data_update_batch.R
@@ -103,11 +103,8 @@ gc()
 
 ### Step 5 Peak prediction forecasting 
 
-<<<<<<< HEAD
 source (paste0(dir_scripts,"CLIC_Brazil_peak_pred_function.R"),echo=TRUE)
-=======
- source (paste0(dir_scripts,"CLIC_Brazil_peak_pred_function.R"),echo=TRUE)
->>>>>>> cb44b7b5173946b4745d69b1bb2a8ed749c494b6
+
 # 
 today <- Sys.Date()
 today <- format(today, format="%d-%B-%Y")
@@ -176,14 +173,14 @@ closeAllConnections()
 
 ### Step 9   Deploy app 
 
-source (paste0(dir_scripts,"CLIC_Brazil_deploy_app.R"),echo=TRUE)
-
-today <- Sys.Date()
-today <- format(today, format="%d-%B-%Y")
-log_file <- paste0(log_fil_dir,"br_data_batch", today,".log")
-sink(file=log_file,append=TRUE)
-print("Step 9 Deploy the app to the CMMID server")
-now_time <- Sys.time() 
-print(now_time)
-closeAllConnections()
+# source (paste0(dir_scripts,"CLIC_Brazil_deploy_app.R"),echo=TRUE)
+# 
+# today <- Sys.Date()
+# today <- format(today, format="%d-%B-%Y")
+# log_file <- paste0(log_fil_dir,"br_data_batch", today,".log")
+# sink(file=log_file,append=TRUE)
+# print("Step 9 Deploy the app to the CMMID server")
+# now_time <- Sys.time() 
+# print(now_time)
+# closeAllConnections()
 
